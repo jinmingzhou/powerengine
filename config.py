ed53ff9baa91e1162ec60a1cd9bc82fbb56f5988
robin <44176291+jinmingzhou@users.noreply.github.com>
2019-05-16 19:03:56 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-STARTHEADER={
-    'Cookie':'part=stable; tt_webid=6691515224187782660; csrftoken=3l4jkfHeTjSpzolTblbcaW4WMrGYbfRn; gr_user_id=f8d9c77f-d452-4861-aab4-90626fb0c8bf; aefa4e5d2593305f_gr_session_id=98090fd2-9be0-4603-9e6e-beb5d170cae1; grwng_uid=1f9354bc-3b22-4aff-b286-ddc765d85c76; aefa4e5d2593305f_gr_session_id_98090fd2-9be0-4603-9e6e-beb5d170cae1=true; login_flag=5e83b063a10d46c903897d8e82bedf7f; gr_session_id_9194a4ba15b63f36=71ed9aa6-4b9c-4430-82c3-d6795e779136; gr_cs1_71ed9aa6-4b9c-4430-82c3-d6795e779136=advertiser_id%3A109101412129; gr_session_id_9194a4ba15b63f36_71ed9aa6-4b9c-4430-82c3-d6795e779136=true',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-}
-
